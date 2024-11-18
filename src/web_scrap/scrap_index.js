33172f460e279986ab6030367af7ddc1368f820c
Bohdan <103576359+BogdanChumak96@users.noreply.github.com>
2024-11-18 09:08:30 +0000

    Update scrap_index.js

--- src/web_scrap/scrap_index.js
+++ src/web_scrap/scrap_index.js
@@ -13,6 +13,7 @@ export async function scrape() {
         const browser = await puppeteer.launch({
             headless: true, 
             defaultViewport: { width: 1920, height: 1080, zoom: 0.1 }, 
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
 
         const page = await browser.newPage();
